2a59db1bf7f8a67c2253ca92adfe2beb482d9cd8
Hardik Bhimani <hardikbhimani@rocketmail.com>
2023-10-22 13:58:55 +0530

    Added support for authors and isbn

--- main.py
+++ main.py
@@ -6,11 +6,28 @@ from libgen.search_request import SearchRequest
 from libgen.downloader import Downloader
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-q", "--query", help="Search Query")
+parser.add_argument("-title", "--title", help="Title")
+parser.add_argument("-isbn", "--isbn", help="ISBN")
+parser.add_argument("-author", "--author", help="Author")
 args = parser.parse_args()
 
-if args.query and (query := args.query):
-    url_data = URLData(query=query)
+
+def get_kwargs(args):
+    if args.isbn is not None:
+        return {"query": str(args.isbn), "field": "identifier"}
+    elif args.author is not None:
+        return {"query": str(args.author), "field": "author"}
+    elif args.title is not None:
+        return {"query": str(args.title)}
+    else:
+        return None
+
+
+kwargs = get_kwargs(args)
+print(kwargs)
+
+if kwargs is not None:
+    url_data = URLData(**kwargs)
     url = URLBehavior(url_data).get_url_filtered()
     records = SearchRequest(url).aggregate_request_data()
     links = DownloadLinks(records).resolve_download_links()
